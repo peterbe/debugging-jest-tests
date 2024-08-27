9a9a68e6c4cf7d041b1c9b0018b5438f1e7c9c1f
Peter Bengtsson <mail@peterbe.com>
2024-08-27 17:37:43 -0400

    Jest test React rendering

---
+++ src/fetch.tsx
@@ -0,0 +1,3 @@
+export function Fetch() {
+  return <div>Hello world</div>;
+}
--- src/flaky.ts
+++ src/flaky.ts
@@ -2,7 +2,7 @@ export async function flaky() {
   //   if (Math.random() < 0.005) {
   //     throw new Error("Flaky error");
   //   } else if (Math.random() > 0.05) {
-  console.error("ERRORORORORO");
+  //   console.error("ERRORORORORO");
   //   } else if (Math.random() > 0.5) {
   //     console.warn("WARNWARNWARN");
   //   }
