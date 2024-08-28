a224f8ebc414b0d05572388ff43d189976891841
Peter Bengtsson <mail@peterbe.com>
2024-08-27 21:01:50 -0400

    globally scoped caching of console error

--- src/flaky.ts
+++ src/flaky.ts
@@ -1,8 +1,12 @@
+let hasErrored = false;
 export async function flaky() {
   //   if (Math.random() < 0.005) {
   //     throw new Error("Flaky error");
   //   } else if (Math.random() > 0.05) {
-  console.error("ERRORORORORO");
+  if (!hasErrored) {
+    console.error("ERRORORORORO");
+    hasErrored = true;
+  }
   //   } else if (Math.random() > 0.5) {
   //     console.warn("WARNWARNWARN");
   //   }
